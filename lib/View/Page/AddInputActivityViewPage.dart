f6439b8c760d13b3c8ad258d5cbe81036db5bb23
Chondro Satrio Wibowo <chondrosatriowibowo@gmail.com>
2021-10-06 23:55:35 +0700

    Update Sticker

--- lib/View/Page/AddInputActivityViewPage.dart
+++ lib/View/Page/AddInputActivityViewPage.dart
@@ -1,43 +1,174 @@
 import 'package:flutter/material.dart';
 import 'package:regenki/Shared/Theme.dart';
 
-class AddInputViewPage extends StatelessWidget {
+class AddInputViewPage extends StatefulWidget {
   //const AddInputViewPage({Key? key}) : super(key: key);
+  @override
+  State<AddInputViewPage> createState() => _AddInputViewPageState();
+}
+
+class _AddInputViewPageState extends State<AddInputViewPage> {
   TextEditingController titleController = TextEditingController();
+
+  String categoryText = "Dancing";
+
+  DateTime dateData = DateTime.now();
+  TimeOfDay timeData = TimeOfDay.now();
+
+  selectDateWidget(BuildContext context) async {
+    final DateTime? selected = await showDatePicker(
+        context: context,
+        initialDate: dateData,
+        firstDate: DateTime(2020),
+        lastDate: DateTime(2030),helpText: "Find your Activity",);
+    if (selected != null && selected != dateData) {
+      setState(() {
+        dateData = selected;
+      });
+
+    }
+  }
+  selectTimeWidget(BuildContext context) async {
+    final TimeOfDay? selected = await showTimePicker(context: context, initialTime: timeData,helpText: "Set times of your activity"
+     );
+    if (selected != null && selected != timeData) {
+      setState(() {
+        timeData = selected;
+      });
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 48, left: 24, right: 24),
-      color: Colors.white,
-      child: Column(
-        children: [
-          Container(
-            child: Row(
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 48, left: 24, right: 24),
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Cancel",
+                        style: poppinsWhiteColor.copyWith(
+                            fontSize: 14,
+                            color: Colors.red,
+                            fontWeight: FontWeight.w700),
+                      )),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Save",
+                        style: poppinsWhiteColor.copyWith(
+                            fontSize: 14,
+                            color: Colors.green,
+                            fontWeight: FontWeight.w700),
+                      ))
+                ],
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Title"),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Cancel",
-                      style: poppinsWhiteColor.copyWith(
-                          fontSize: 14,
-                          color: Colors.red,
-                          fontWeight: FontWeight.w700),
-                    )),
-                TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Save",
-                      style: poppinsWhiteColor.copyWith(
-                          fontSize: 14,
-                          color: Colors.green,
-                          fontWeight: FontWeight.w700),
-                    ))
+                Text(
+                  "Category",
+                  style: poppinstextColor.copyWith(
+                      color: Colors.black, fontSize: 16),
+                ),
+                DropdownButton<String>(
+                  onChanged: (String? value) {
+                    setState(() {
+                      categoryText = value!;
+                    });
+
+                  },
+                  items: <String>[
+                    "Dancing",
+                    "Walking",
+                    "Cleaning",
+                    "Running",
+                    "Gaming",
+                    "Learning",
+                    "Working",
+                    "Others"
+                  ].map<DropdownMenuItem<String>>((String e) {
+                    return DropdownMenuItem(value: e, child: Text(e));
+                  }).toList(),
+                  value: categoryText,
+                )
               ],
             ),
-          ),
+            categoryText == "Others" ? TextField(
+              decoration: InputDecoration(hintText: "Category"),
+            ) : SizedBox(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Date Activity"),
+                TextButton(
+                    style: TextButton.styleFrom(backgroundColor: primaryColor),
+                    onPressed: () {
+                      selectDateWidget(context);
+                    },
+                    child: Text(
+                        "${dateData.day}/${dateData.month}/${dateData.year}",style: poppinstextColor,))
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Time Activity"),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: primaryColor),
+                    onPressed: () {
+                      selectTimeWidget(context);
+                    },
+                    child: Text(
+                        "${timeData.hour}:${timeData.minute}",style: poppinstextColor,))
+              ],
+            ),Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Category",
+                  style: poppinstextColor.copyWith(
+                      color: Colors.black, fontSize: 16),
+                ),
+                DropdownButton<String>(
+                  onChanged: (String? value) {
+                    setState(() {
+                      categoryText = value!;
+                    });
 
-        ],
+                  },
+                  items: <String>[
+                    "Dancing",
+                    "Walking",
+                    "Cleaning",
+                    "Running",
+                    "Gaming",
+                    "Learning",
+                    "Working",
+                    "Others"
+                  ].map<DropdownMenuItem<String>>((String e) {
+                    return DropdownMenuItem(value: e, child: Text(e));
+                  }).toList(),
+                  value: categoryText,
+                )
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
