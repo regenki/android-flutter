b643141df590b67626456872cee6098d6298ccad
Chondro Satrio Wibowo <chondro.satrio@gramedia.id>
2024-02-06 09:42:04 +0700

    Fastlane setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:regenki/View/Page/ActivityViewPage.dart';
-import 'package:regenki/View/Page/AddInputActivityViewPage.dart';
 import 'package:regenki/Bloc/Activity/ActivityServiceCubit.dart';
 
 import 'package:regenki/View/Page/OnBoardingViewPage.dart';
